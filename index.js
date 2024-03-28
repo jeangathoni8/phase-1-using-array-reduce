00030c888f2c37e32518b897ba285742ea1a1f87
jeangathoni8 <jeangathoni39@gmail.com>
2024-03-28 15:40:21 +0200

    array reduce lab

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const totalBatteries = batteryBatches.reduce(getSumBatteries);
+
+function getSumBatteries(acc, curr){
+     return acc + curr;
+}
+
+console.log(totalBatteries);
+
+// Code your solution here
